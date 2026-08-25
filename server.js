afa393b68f7bedcd9e07f1d348cfd0efe5ead85e
lazerwaiz-lab <lazerwaiz@gmail.com>
2026-08-25 22:54:31 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(cors({
 app.use(express.json({ limit: '2mb' }));
 
 // 3. Sert tous les fichiers HTML/JS/CSS à la racine de room-checker-service
-app.use(express.static(__dirname));
+//app.use(express.static(__dirname));
 
 // 4. Route d'accueil
 app.get('/', (req, res) => {
